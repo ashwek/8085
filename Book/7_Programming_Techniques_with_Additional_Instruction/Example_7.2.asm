5ae2d8a4b70fae6b18da51e597cca95b8a6cc8b3
ashwek <ashwekswamy@protonmail.com>
2018-12-04 00:25:32 +0530

    Create Example_7.2.asm

---
+++ Book/7_Programming_Techniques_with_Additional_Instruction/Example_7.2.asm
@@ -0,0 +1,9 @@
+;Write instruction to load 16-bit number 2050H in the register pair HL using LXI
+;& MVI opcodes, & explain the difference between the 2 inst
+
+LXI H, 2050H
+
+MVI H, 20H
+MVI L, 05H
+
+HLT
